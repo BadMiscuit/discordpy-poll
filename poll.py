f7b940479aa81fe8208dff06c6ca7096db1ebfcb
badmiscuit <tr.nguyenanh@gmail.com>
2020-12-06 22:38:06 +0000

    Overhaul code to poll, removed db support

--- poll.py
+++ poll.py
@@ -1,146 +1,147 @@
 import discord
-from config import POLL_CHANNEL, TEST_CHANNEL, PEPEBAD
-from classes import *
 
-alphabet = ['\N{REGIONAL INDICATOR SYMBOL LETTER A}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER B}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER C}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER D}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER E}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER F}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER G}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER H}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER I}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER J}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER K}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER L}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER M}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER N}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER O}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER P}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER Q}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER R}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER S}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER T}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER U}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER V}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER W}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER X}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER Y}',
-        '\N{REGIONAL INDICATOR SYMBOL LETTER Z}']
+from discord.ext import commands
+from utils.util import srs_only, poll_only, logtrace
+from utils.emojis import alphabet, pepeangry, pepecry
+from utils.config import GUILD_ID
+from .classes import Poll, Option
 
-async def send_poll(ctx, *args, msg=""):
-    if (ctx.message.channel.id != POLL_CHANNEL
-            and ctx.message.channel.id != TEST_CHANNEL):
-        return
-    if (len(args) == 0):
-        await ctx.send(embed=man_poll())
-        return
-    try:
-        if (len(args) > 27):
-            await ctx.send("Arrête d'essayer de me casser {0}".format(PEPEBAD))
-            return
-        poll = create_poll(args)
-        if (len(poll.options) == 1):
+
+class PollCog(commands.Cog):
+    """
+    Simple poll.
+    Commands:
+      * /poll "Question?"
+      * /poll "Question?" "Value 1" "Value 2"
+      * /poll_add "New value"
+    """
+    last_poll = Poll()
+
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command(name='poll')
+    @srs_only()
+    @poll_only()
+    async def poll(self, ctx, *args):
+        try:
+            await self.send_poll(ctx, *args)
+        except Exception as e:
+            await logtrace(ctx, e)
+            await ctx.message.add_reaction(self.bot.get_emoji(pepeangry[1]))
+
+
+    @commands.command(name='poll_all')
+    @srs_only()
+    @poll_only()
+    async def poll_all(self, ctx, *args):
+        try:
+            if (len(args) != 0):
+                await self.send_poll(ctx, *args, msg="@here")
+            else:
+                await self.send_poll(ctx, *args)
+        except Exception as e:
+            await logtrace(ctx, e)
+            await ctx.message.add_reaction(self.bot.get_emoji(pepeangry[1]))
+
+
+    @commands.command(name='poll_add')
+    @srs_only()
+    @poll_only()
+    async def poll_add(self, ctx, *args):
+        try:
+            await self.add_option_poll(ctx, *args)
+        except Exception as e:
+            await logtrace(ctx, e)
+            await ctx.message.add_reaction("\N{Cross Mark}")
+            await ctx.send("Arrête d'essayer de me casser <:{0}:{1}>".format(pepecry[0], pepecry[1]))
+
+
+    async def send_poll(self, ctx, *args, msg=""):
+        """ Args parser and dispatch method for /poll and /poll_all """
+        if (len(args) == 0):
+            await ctx.send(embed=self._help())
+        elif (len(args) > 20):
+            await ctx.send("Pas plus de 20 options <:{0}:{1}>".format(pepecry[0], pepecry[1]))
+        elif (len(set(args[1:])) == 1):
             await ctx.send("Crée un sondage avec des options différentes")
-            return
-        msg = await ctx.send(content=msg, embed=poll.to_embed())
-        set_poll(msg.id, poll)
-        for option in poll.options:
-            await msg.add_reaction(option.emoji)
-    except Exception as e:
-        print(str(e))
-        await ctx.send("Arrête d'essayer de me casser {0}".format(PEPEBAD))
+        else:
+            self.last_poll = self.create_poll(title=args[0], options=args[1:])
+            msg = await ctx.send(content=msg, embed=self.last_poll.to_embed())
+            self.last_poll._id = msg.id
+            for option in self.last_poll.options:
+                await msg.add_reaction(option.emoji)
 
-def create_poll(args):
-    poll = Poll(title=args[0])
-    #Simple Poll
-    if (len(args) == 1):
-        poll.add_option(Option(emoji="\N{Thumbs Up Sign}"))
-        poll.add_option(Option(emoji="\N{Thumbs Down Sign}"))
-    #Simple Poll 2
-    elif (len(args) == 2):
-        poll.title = ""
-        poll.add_option(Option(emoji=alphabet[0], option=args[0]))
-        poll.add_option(Option(emoji=alphabet[1], option=args[1]))
-    #Multiple options Poll
-    else:
-        j = 0
-        for i in range (0, len(args) - 1):
-            option = Option(emoji=alphabet[j], option=args[i + 1])
-            if (option_exists(poll, option)):
-                continue
-            poll.add_option(option)
-            j += 1
-    return poll
 
-async def poll_add_option(ctx, *args):
-    if (ctx.message.channel.id != POLL_CHANNEL
-            and ctx.message.channel.id != TEST_CHANNEL):
-        return
-    # Man
-    if (len(args) == 0):
-        await ctx.send(embed=man_poll_add())
-        return
-    try:
-        poll_id, poll = get_poll()
-        if (poll is None):
+    def create_poll(self, title="", options=set()):
+        poll = Poll(title=title)
+        #Simple Poll
+        if (len(options) == 0):
+            poll.add_option(Option(emoji="\N{Thumbs Up Sign}"))
+            poll.add_option(Option(emoji="\N{Thumbs Down Sign}"))
+        #Simple Poll 2
+        elif (len(options) == 2):
+            poll.add_option(Option(emoji=alphabet[0], option=options[0]))
+            poll.add_option(Option(emoji=alphabet[1], option=options[1]))
+        #Multiple options Poll
+        else:
+            for i in range (0, len(options)):
+                poll.add_option(Option(emoji=alphabet[i], option=options[i]))
+        return poll
+
+
+    async def add_option_poll(self, ctx, *args):
+        if (len(args) == 0):
+            await ctx.send(embed=self._help_add())
+        elif (self.last_poll is None):
             await ctx.send("Il n'y a pas de dernier sondage")
-            return
-        msg = await ctx.fetch_message(poll_id)
-        if (msg == None):
-            await ctx.send("Je ne trouve pas le dernier sondage")
-            return
-        if (poll.options[0].option == "" and poll.options[1].option == ""):
-            poll.clear_options()
-            remove_options(poll_id)
-            for reaction in msg.reactions:
-                await reaction.clear()
-        #if (msg.channel != ctx.channel):
-        #    await ctx.send("Impossible d'éditer un poll d'un autre channel")
-        #    return
-        options = [x.option for x in poll.options]
-        new_options = [x for x in args if x not in options]
-        #FIXME
-        if (len(poll.options) + len(new_options) > 26):
-            await ctx.send("Arrête d'essayer de me casser {0}".format(PEPEBAD))
-            return
-        # Option exists
-        if (len(new_options) == 0):
-            await ctx.send("Rien à ajouter")
-            return
-        for opt in new_options:
-            emoji = alphabet[len(poll.options)]
-            add_new_option(poll_id, emoji, opt)
-            poll.add_option(Option(option=opt, emoji=emoji))
-            await msg.add_reaction(emoji)
-        await msg.edit(embed=poll.to_embed())
-        await ctx.message.add_reaction("\N{White Heavy Check Mark}")
-    except Exception as e:
-        print(str(e))
-        await ctx.message.add_reaction("\N{Cross Mark}")
-        await ctx.send("Arrête d'essayer de me casser {0}".format(PEPEBAD))
+        else:
+            msg = await ctx.fetch_message(self.last_poll._id)
+            if (msg == None):
+                await ctx.send("Je ne trouve pas le dernier sondage")
+            elif (self.last_poll.options[0].option == "" and \
+                    self.last_poll.options[1].option == ""):
+                self.last_poll.clear_options()
+                for reaction in msg.reactions:
+                    await reaction.clear()
+            options = [x.option for x in self.last_poll.options]
+            new_options = [x for x in args if x not in options]
+            if (len(self.last_poll.options) + len(new_options) > 20):
+                await ctx.send("Arrête d'essayer de me casser <:{0}:{1}>".format(\
+                        pepecry[0], pepecry[1]))
+                return
+            # Option exists
+            if (len(new_options) == 0):
+                await ctx.send("Rien à ajouter")
+                return
+            for opt in new_options:
+                emoji = alphabet[len(self.last_poll.options)]
+                self.last_poll.add_option(Option(option=opt, emoji=emoji))
+                await msg.add_reaction(emoji)
+            await msg.edit(embed=self.last_poll.to_embed())
+            await ctx.message.add_reaction("\N{White Heavy Check Mark}")
 
-def option_exists(poll, option):
-    for o in poll.options:
-        if (o.option == option.option):
-            return True
-    return False
 
-def man_poll():
-    return discord.Embed(colour=discord.Colour.from_rgb(254, 254, 254),
-        title="man /poll",
-        description="""
-        **Sondage simple \N{Thumbs Up Sign}/\N{Thumbs Down Sign}:**
-        /poll \"Suis-je un bon bot ?\"
-        ou
-        /poll \"Vim\" \"Emacs\"\n
-        **Sondage avec plusieurs propositions**
-        /poll \"Pain au chocolat ou chocolatine ?\" \"Pain au chocolat\" \"Pain au chocolat\"
-        """)
+    def _help(self):
+        return discord.Embed(colour=discord.Colour.from_rgb(254, 254, 254),
+            title="Manuel d'utilisation",
+            description="""
+            **Sondage simple \N{Thumbs Up Sign}/\N{Thumbs Down Sign}:**
+            /poll \"Suis-je un bon bot ?\"
+            ou
+            /poll \"Vim\" \"Emacs\"\n
+            **Sondage avec plusieurs propositions**
+            /poll \"Pain au chocolat ou chocolatine ?\" \"Pain au chocolat\" \"Pain au chocolat\"
+            """)
 
-def man_poll_add():
-    return discord.Embed(colour=discord.Colour.from_rgb(254, 254, 254),
-        title="man /poll_add",
-        description="""**Pour ajouter une option au dernier sondage**
-        /poll_add \"Option 1\" \"Option 2\" \"Option 3\" ... """)
+    def _help_add(self):
+        return discord.Embed(colour=discord.Colour.from_rgb(254, 254, 254),
+            title="Manuel d'utilisation",
+            description="""**Pour ajouter une option au dernier sondage**
+            /poll_add \"Option 1\" \"Option 2\" \"Option 3\" ... """)
+
+
+def setup(bot):
+    bot.add_cog(PollCog(bot))
